03428bd10f597d6fcd13d4d056d96a0b7094e111
Наиль <fano2458@gmail.com>
2023-06-11 22:00:25 +0600

    todo

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from datetime import datetime
-from typing import Optional
-
-from fasapi.encoders import jsonable_encoder
-from model.model import Task, TaskList
+from fastapi.encoders import jsonable_encoder
+from model.model import Task
 import model.taskman as taskman
 
 
@@ -12,24 +9,28 @@ app = FastAPI()
 
 @app.get("/api/tasks")
 async def get_tasks():
-	return "TODO"
+	return await taskman.get_tasks()
 
 
-@app.get("api/tasks/{id}")
+@app.get("/api/tasks/{id}")
 async def get_task(id: int):
-	return "TODO"
+	return await taskman.get_tasks(id)
 
 
 @app.post("/api/tasks/create")
 async def create_task(task: Task):
-	return "TODO"
+	id = await taskman.create_task(task)
+	return await taskman.get_tasks(id)
 
 
 @app.put("/api/tasks/{id}/update")
 async def update_task(id: int, task: Task):
-	return "TODO"
+	await taskman.update_task(id, task)
+	return await taskman.get_tasks(id)
 
 
 @app.delete("/api/tasks/{id}/delete")
 async def delete_task(id: int):
-	return "TODO"
+	id = await taskman.delete_task(id)
+	response = {id:"Task successfully deleted"}
+	return jsonable_encoder(response)
--- model/taskman.py
+++ model/taskman.py
@@ -8,20 +8,43 @@ filepath = "data/tasks.json"
 
 
 async def data_to_json(data: List):
-	pass
+	data = json.dumps(data)
+	with open(filepath, "w") as file:
+		file.write(data)
 
 
 async def get_tasks(id: Optional[int]=0):
-	return "response"
+	tasks = parse_file_as(List[TaskList],"data/tasks.json")
+	data = {task.id: task.dict() for task in tasks}
+	try:
+		response = data if id == 0 else data[id]
+	except KeyError:
+		response = data
+	return response
 
 
 async def create_task(new_task: Task):
-	return "id"
+	tasks = parse_file_as(List[TaskList],"data/tasks.json")
+	id = max([task.id for task in tasks])+1
+	tasks.append(TaskList(id=id,task=new_task))
+	data = [task.dict() for task in tasks]
+	await data_to_json(data)
+	return id
 
 
 async def delete_task(id):
-	return "id"
+	tasks = parse_file_as(List[TaskList],"data/tasks.json")
+	tasks = [task for task in tasks if task.id != id]
+	data = [task.dict() for task in tasks]
+	await data_to_json(data)
+	return id
 
 
 async def update_task(id: int, new_task: Task):
-	return "id"
+	tasks = parse_file_as(List[TaskList],"data/tasks.json")
+	data = [task.dict() for task in tasks]
+	for task in data:
+		if task["id"] == id:
+			task["task"] = new_task.dict()
+	await data_to_json(data)
+	return id
